01334c85dfdfc72150a03065de52516677dcece6
KENNEDY KAMONDE <kamonde.kennedy@gmail.com>
2021-09-07 10:42:18 +0300

    Changed the firebase enviroments to production mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCYv7ON2IC-tZ4uvRuZzbhGTZWt9wd4Vdo",
+    authDomain: "adrian-kenya.firebaseapp.com",
+    projectId: "adrian-kenya",
+    storageBucket: "adrian-kenya.appspot.com",
+    messagingSenderId: "260345886516",
+    appId: "1:260345886516:web:d846feb927b3e722565761"
+  }
 };
